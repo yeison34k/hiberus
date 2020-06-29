5c1b3ff1e51998e007ed0314919d4ad53f0246ba
yeison34k <yaristizabal34k@gmail.com>
2020-06-29 17:34:13 -0500

    fix and performance

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # hiberus
  The main goal of the test it's to simulate a tyipical checkout operation in an ecommerce solution
+
+
+
+## payment api
+### swagger
+http://localhost:8181/swagger-ui.html
+
+run docker payment
+
+mvn clean package
+
+docker build -t payment:v1 .
+
+docker run -p 8181:8181 --name payment --network hiberus payment:v1
+
+
+## order api
+## swagger
+http://localhost:8086/swagger-ui.html
+
+run docker order
+
+mvn clean package
+
+docker build -t order:v1 .
+
+docker run -p 8086:8086 --name payment --network hiberus payment:v1
+
+
+## bill api
+### swagger 
+http://localhost:8092/swagger-ui.html
+
+run docker bill
+
+mvn clean package
+
+docker build -t bill:v1 .
+
+docker run -p 8092:8092 --name bill-simulation --network hiberus payment:v1
+
+
+## logistic api
+### swagger
+http://localhost:8091/swagger-ui.html
+
+run docker order
+
+mvn clean package
+
+docker build -t logistic:v1 .
+
+docker run -p 8092:8092 --name logistic-simulation --network hiberus logistic:v1
--- payment/src/main/java/com/payment/consts/AppConsts.java
+++ payment/src/main/java/com/payment/consts/AppConsts.java
@@ -2,8 +2,6 @@ package com.payment.consts;
 
 public class AppConsts {
 	
-	public static final String BASE = "http://localhost:";
-	
 	public static final String ORDER_PORT = "8086";
 		
 	public static final String SEND_ORDER_PORT = "8091";
@@ -12,11 +10,11 @@ public class AppConsts {
 	
 	public static final String CLIENTID= "clientId";
 	
-	public static final String CREATE_ORDER_URL = BASE + ORDER_PORT + "/order/create";
+	public static final String CREATE_ORDER_URL = "http://order-simulation:" + ORDER_PORT + "/order/create";
 	
-	public static final String CREAE_SEND_ORDER_URL = BASE + SEND_ORDER_PORT + "/send/create";
+	public static final String CREAE_SEND_ORDER_URL = "http://logistic-simulation:" + SEND_ORDER_PORT + "/send/create";
 	
-	public static final String CREAE_BILL_ORDER_URL = BASE + BILL_ORDER_PORT + "/bill/create";	
+	public static final String CREAE_BILL_ORDER_URL = "http://bill-simulation:" + BILL_ORDER_PORT + "/bill/create";	
 	
 	public static final String DATE = "date";
 	
@@ -31,7 +29,6 @@ public class AppConsts {
 	public static final String BILL = "bill";
 	
 	public static final String ENVIADO = "enviado";
-	
 	
 	public static final String NUMEBER_BILL = "numberBill";
 	
