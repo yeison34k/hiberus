0e0ebe4f06a2f19dff5464f32dbdc7694ad7ef43
yeison34k <yaristizabal34k@gmail.com>
2020-07-24 21:26:13 -0500

    update app

--- hiberus_app/lib/screens/details/components/body.dart
+++ hiberus_app/lib/screens/details/components/body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hiberusapp/constants.dart';
 import 'package:hiberusapp/models/Product.dart';
 import 'package:hiberusapp/screens/details/components/product_title_with_image.dart';
 
@@ -19,10 +20,41 @@ class Body extends StatelessWidget {
                             children: <Widget>[
                                 Container(
                                     margin: EdgeInsets.only(top: size.height * 0.4),
+                                    padding: EdgeInsets.only(top: size.height * 0.13, left: kDefaultPaddin, right: kDefaultPaddin),
                                     height: 500,
                                     decoration: BoxDecoration(
                                         color: Colors.white,
                                         borderRadius: BorderRadius.only(topLeft: Radius.circular(24), topRight:  Radius.circular(24))
+                                    ),
+                                    child: Column(
+                                        children: <Widget>[
+                                            Row(
+                                                children: <Widget>[
+                                                    Column(
+                                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                                        children: <Widget>[
+                                                            Text("Color"),
+                                                            Row(
+                                                                children: <Widget>[
+                                                                    ColorDot(color: Color(0xff356c95), isSeleted: true),
+                                                                    ColorDot(color: Color(0xff959323)),
+                                                                    ColorDot(color: Color(0xff674366))
+                                                                ],
+                                                            )
+                                                        ],
+                                                    ),
+                                                    RichText(
+                                                        text: TextSpan(
+                                                            style: TextStyle(color: kTextColor),
+                                                            children: [
+                                                                TextSpan(text: "Size\n"),
+                                                                TextSpan(text: "${product.size} cm"),
+                                                            ],
+                                                        ),
+                                                    )
+                                                ],
+                                            )
+                                        ],
                                     ),
                                 ),
                                 ProductTitleWithImage(product: product)
@@ -34,4 +66,34 @@ class Body extends StatelessWidget {
         );
     }
 
+}
+
+
+class ColorDot extends StatelessWidget {
+    final Color color;
+    final bool isSeleted;
+
+  const ColorDot({Key key, this.color, this.isSeleted = false}) : super(key: key);
+
+    @override
+    Widget build(BuildContext context) {
+        return Container(
+            margin: EdgeInsets.only(top: kDefaultPaddin / 4, right: kDefaultPaddin / 2),
+            padding: EdgeInsets.all(2.5),
+            height: 24,
+            width: 24,
+            decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(
+                    color: isSeleted ? color : Colors.transparent
+                )
+            ),
+            child: DecoratedBox(
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: color
+                ),
+            ),
+        );
+    }
 }
